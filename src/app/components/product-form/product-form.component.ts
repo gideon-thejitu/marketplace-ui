70de39c8d2cb0b74f0dd5acf21036a36cc17ad4b
gideon-thejitu <gideon.kimutai@thejitu.com>
2023-09-01 05:15:02 +0300

    refactor: extract product component

--- src/app/components/product-form/product-form.component.ts
+++ src/app/components/product-form/product-form.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Input, OnInit, Output, OnChanges, SimpleChanges, SimpleChange} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output, OnChanges, SimpleChange} from '@angular/core';
 import {FormBuilder, Validators} from "@angular/forms";
 import {CategoriesService} from "../../services/categories.service";
 import {Category} from "../../interfaces/category";
